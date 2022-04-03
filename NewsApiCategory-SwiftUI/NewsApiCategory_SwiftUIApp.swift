0d1806d0a1f92548bffde793298f149bd89b3d4b
Kaan İzgi <kaanizgi1531@gmail.com>
2022-04-03 12:27:22 +0300

    Initial Commit

---
+++ NewsApiCategory-SwiftUI/NewsApiCategory_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiCategory_SwiftUIApp.swift
+//  NewsApiCategory-SwiftUI
+//
+//  Created by Kaan İzgi on 3.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiCategory_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
